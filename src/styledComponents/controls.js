8013877d093b14ef9500b656d8df06e09e49b037
cloudstrife9494 <aymansmimah@yahoo.fr>
2018-11-18 09:42:49 +0000

    add app controls

---
+++ src/styledComponents/controls.js
@@ -0,0 +1,33 @@
+import {colors} from './colors';
+
+
+
+export const AppButton= styled.button`
+    border: none;
+    background: ${colors.light_grey};
+    width: 128px;
+    height: 32px;
+    color: ${colors.white}
+
+    &:focus{
+        background: ${colors.dark_orange};
+    }
+`
+
+export const AppAnchor= styled.a`
+   border: none;
+   backgound: none;
+   color: ${colors.light_orange};
+
+   &:hover{
+       color: ${colors.dark_orange};
+   }
+`
+
+export const DefaultAnchor= styled.a`
+   margin:0;
+   padding:0;
+   border: none;
+   background: none;
+   text-decoration: none;
+`
