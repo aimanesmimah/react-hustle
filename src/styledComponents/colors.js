f1abdaf65b3b00778de3f0cea799950d8154a3a2
cloudstrife9494 <aymansmimah@yahoo.fr>
2018-11-18 09:39:23 +0000

    add fonts & colors

---
+++ src/styledComponents/colors.js
@@ -0,0 +1,7 @@
+export const colors = Object.freeze({
+    light_grey: '#EDEDED',
+    dark_grey: '#4A4A4A',
+    light_orange: '#EA7F28',
+    dark_orange: '#D37324',
+    white: '#FFF'
+})
---
+++ src/styledComponents/fonts.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Huge= styled.p`
+   font-size: 32px;
+   font-weight: bolder;
+`
+
+export const BigBold= styled.p`
+   font-size: 18px;
+   font-weight: bold;
+`
+
+export const BigRegular= styled.p`
+    font-size: 18px;
+    font-weight: normal;
+`
+
+export const Medium= styled.p`
+    font-size: 14px;
+    font-weight: normal;    
+`
+
+export const Small= styled.p`
+    font-size: 12px;
+    font-weight: normal;
+`
